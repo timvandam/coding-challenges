79fa3219530c04a4791b11d739bdce6e5c27ae54
Tim van Dam <timvandamcs@gmail.com>
2020-08-17 21:57:20 +0200

    small refactoring

--- src/LeetCode/396. Rotate Function/index.ts
+++ src/LeetCode/396. Rotate Function/index.ts
@@ -1,4 +1,6 @@
 /**
+ * DIFFICULTY: Medium
+ *
  * Given an array of integers A and let n to be its length.
  * Assume Bk to be an array obtained by rotating the array A k positions clock-wise, we define a "rotation function" F on A as follow:
  * F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1].
@@ -7,21 +9,16 @@
 
 export function maxRotateFunction (arr: number[]): number {
 	const arraySum = arr.reduce((x, y) => x + y, 0)
-	let sum = F(arr)
+	let sum = arr.reduce((acc, num, i) => acc + i * num, 0) // initial sum
 	let max = sum
 	for (let i = 0; i < arr.length - 1; i++) {
 		// Try all combinations
-		// The next combination adds one of each entry (because their index gets incremented), but the last entry (arr.length - 1 - i) becomes zero.
-		// Since it becomes zero we should subtract it (arr.length - 1) (maximum index) times. Instead I do it once more, but also add once more in the reduce statement.
-		// This is simply due to code cleanliness
-		sum = sum + arraySum - arr.length * arr[arr.length - 1 - i]
-		console.log(sum)
+		// The next combination adds one of each entry (because their index gets incremented), but the last entry (at index arr.length - 1 - i) becomes zero.
+		// Since it becomes zero we should subtract it (arr.length - 1) (maximum index) times. Because arraySum already adds this number once, I subtract it one more time,
+		// making me subtract (arr.length) times
+		sum += arraySum - arr.length * arr[arr.length - 1 - i]
 		max = Math.max(max, sum)
 	}
 
 	return max
-}
-
-function F (arr: number[]) {
-	return arr.reduce((acc: number, num: number, i: number) => acc + i * num, 0)
 }
