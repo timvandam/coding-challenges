f9e79620a65b838f0afc9bc5809ca25535f12afb
Tim van Dam <timvandamcs@gmail.com>
2020-08-28 10:32:54 +0200

    improved space complexity instead of doing recursion from the bottom to the top of the triangle, im now building it from the top to the bottom. this means that were only using one stack frame, instead of n stack frames

--- src/Discord TUD/Pascal's Triangle/index.test.ts
+++ src/Discord TUD/Pascal's Triangle/index.test.ts
@@ -12,7 +12,8 @@ it.each([
 		[1, 19, 171, 969, 3876, 11628, 27132, 50388, 75582, 92378, 92378, 75582, 50388, 27132, 11628, 3876, 969, 171, 19, 1]
 	]
 ])('returns row %i', (i, row) => {
-	expect(pascal(i)).toEqual(row)
+	const result = pascal(i)
+	expect(result[result.length - 1]).toEqual(row)
 })
 
 it('returns something with 999', () => {
--- src/Discord TUD/Pascal's Triangle/index.ts
+++ src/Discord TUD/Pascal's Triangle/index.ts
@@ -4,26 +4,19 @@
  */
 
 /**
- * Returns the n-th row of pascal's triangle
+ * Returns up to the the n-th row of pascal's triangle
  *
  * This is O(1 + 2 + 3 + ... + n) = O(n(n+1)/2) = O(n^2)
  */
-export function pascal(row: number): number[] {
+export function pascal(row: number): number[][] {
 	const dp: number[][] = [[1]]
 
-	function go(rows: number): number[] {
-		if (dp[rows]) return dp[rows]
-
-		const previousRow = go(rows - 1)
-		const row = Array(previousRow.length + 1)
-		for (let i = 0; i < row.length; i++) {
-			row[i] = (previousRow[i - 1] ?? 0) + (previousRow[i] ?? 0)
-		}
-
-		dp[rows] = row
-
-		return row
+	for (let i = 1; i <= row; i++) {
+		const previousRow = dp[i - 1]
+		dp[i] = Array(i + 1)
+			.fill(undefined)
+			.map((_, j) => (previousRow[j - 1] ?? 0) + (previousRow[j] ?? 0))
 	}
 
-	return go(row)
+	return dp
 }
